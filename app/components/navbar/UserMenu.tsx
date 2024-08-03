612c63368b58bbb962e3d9bfa313170cc9daa5f0
zeuzmakessoftware <aodude@icloud.com>
2024-08-03 11:40:32 -0700

    new nav bar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,40 +6,35 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
 
-    const toggleOpen = useCallback(() =>{
-        setIsOpen((value) => !value);
+    const toggleOpen = useCallback(() => {
+        setIsOpen(prev => !prev);
     }, []);
 
     return (
         <div className="relative">
-            <div className="flex flex-row items-center gap-3">
-                <div onClick={() => {}} className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer">
-                    Join RecRent!
-                </div>
-                <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
-                    <AiOutlineMenu />
+            <div className="flex items-center gap-3">
+                <button
+                    onClick={toggleOpen}
+                    className="p-4 md:py-1 md:px-3 flex items-center gap-3 rounded-full cursor-pointer border border-white hover:shadow-md hover:border-neutral-200 transition ease-in-out duration-200"
+                >
                     <div className="hidden md:block">
                         <Avatar />
-
+                    </div>
+                    <AiOutlineMenu />
+                </button>
+            </div>
+            {isOpen && (
+                <div className="absolute right-0 top-12 w-[40vw] md:w-[15vw] bg-white rounded-xl shadow-md overflow-hidden text-sm transition ease-in-out">
+                    <div className="flex flex-col cursor-pointer">
+                        <MenuItem onClick={() => {}} label="Login" />
+                        <MenuItem onClick={() => {}} label="Sign up" />
                     </div>
                 </div>
-            </div>
-        {isOpen && (
-            <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-                <div className="flex flex-col cursor-pointer">
-                    <>
-                        <MenuItem onClick={() => {}} label="Login"/>
-                    
-                        <MenuItem onClick={() => {}} label="Sign up"/>
-                    </>
-                </div>
-
-            </div>
-        )}
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default UserMenu;
