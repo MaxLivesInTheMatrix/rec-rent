448c221d7bcadff4e4da43e131df35fe6a93ad82
zeuzmakessoftware <aodude@icloud.com>
2024-08-03 19:41:59 -0700

    listings initial version

---
+++ app/components/Listing.tsx
@@ -0,0 +1,49 @@
+interface ListingProps {
+    title: string;
+    rating: number;
+    reviews: number;
+    location: string;
+    price: number;
+    imageUrl: string;
+    id: number;
+}
+
+const Listing: React.FC<ListingProps> = ({ 
+    title, 
+    rating,
+    reviews,
+    location,
+    price, 
+    imageUrl,
+    id
+}) => {
+    return (
+        <a href="/" className="w-80 rounded-3xl overflow-hidden shadow-lg text-white no-underline hover:bg-neutral-100">
+            <div className="flex flex-col h-full">
+                <div className="w-full h-64">
+                    <img
+                        className="w-full h-full object-cover"
+                        src={imageUrl}
+                        alt="Cool Boat"
+                    />
+                </div>
+                <div className="flex flex-col justify-between flex-grow">
+                    <div className="px-6 py-4 flex-grow">
+                        <div className="font-black text-xl mb-2 text-black">{title}</div>
+                        <p className="text-gray-600 text-base">
+                            {rating} Stars ★ ({reviews} Reviews)
+                        </p>
+                        <p className="text-gray-600 text-base">
+                            {location}
+                        </p>
+                    </div>
+                    <div className="px-6 pt-4 pb-4">
+                        <span className="text-md font-semibold underline text-black">${price} total per day</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    );
+}
+
+export default Listing;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import Listing from "./components/Listing";
+import mock from "../public/mock.json";
+
 export default function Home() {
+
   return (
-    <div className="text-rose-500 text-2xl hidden">Hello Rent-Rec!</div>
+    <div className="pt-28 flex flex-col items-center">
+      <div className="text-rose-500 text-2xl hidden">Hello Rent-Rec!</div>
+      <div className="pt-24 px-8 flex flex-wrap justify-center gap-8 w-full max-w-screen-xl">
+        {mock.map(item => (
+            <Listing title={item.title} rating={item.rating} reviews={item.reviews} location={item.location} price={item.price} imageUrl={item.imageUrl} id={item.id}/>
+        ))}
+        </div>
+    </div>
   );
 }
